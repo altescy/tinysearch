66c87024be280b490da6ae09085acb3c770f3028
altescy <altescy@fastmail.com>
2023-01-18 01:39:17 +0900

    fix typehint

--- tinysearch/tinysearch.py
+++ tinysearch/tinysearch.py
@@ -1,4 +1,4 @@
-from typing import Generic, List, Optional, Sequence, Tuple, TypeVar, Union
+from typing import Generic, List, Literal, Optional, Sequence, Tuple, TypeVar, Union, overload
 
 from tinysearch.indexers import Indexer
 from tinysearch.storages import Storage
@@ -23,22 +23,47 @@ class TinySearch(Generic[Document, Matrix]):
         self.analyzer = analyzer
         self.stopwords = set(stopwords or ())
 
+    @overload
     def search(
         self,
         query: str,
-        topk: Optional[int] = 10,
+        *,
+        topk: Optional[int] = ...,
+    ) -> List[Document]:
+        ...
+
+    @overload
+    def search(
+        self,
+        query: str,
+        *,
+        return_scores: Literal[False],
+        topk: Optional[int] = ...,
+    ) -> List[Document]:
+        ...
+
+    @overload
+    def search(
+        self,
+        query: str,
+        *,
+        return_scores: Literal[True],
+        topk: Optional[int] = ...,
+    ) -> List[Tuple[Document, float]]:
+        ...
+
+    def search(
+        self,
+        query: str,
+        *,
         return_scores: bool = False,
-        return_documents: bool = True,
-    ) -> Union[List[str], List[Tuple[str, float]], List[Document], List[Tuple[Document, float]]]:
+        topk: Optional[int] = 10,
+    ) -> Union[List[Document], List[Tuple[Document, float]]]:
         tokens = list(self.analyzer(query))
         if self.stopwords:
             tokens = [token for token in tokens if token not in self.stopwords]
         query_vector = self.vectorizer.vectorize_queries([tokens])
         results = self.indexer.search(query_vector, topk=topk)[0]
-        if return_scores and return_documents:
+        if return_scores:
             return [(self.storage[id_], score) for id_, score in results]
-        if return_scores and not return_documents:
-            return results
-        if not return_scores and return_documents:
-            return [self.storage[id_] for id_, _ in results]
-        return [id_ for id_, _ in results]
+        return [self.storage[id_] for id_, _ in results]
