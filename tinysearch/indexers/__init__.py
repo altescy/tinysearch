a870c32918c340d59109dba27bfa1e31fcdb9eb6
altescy <altescy@fastmail.com>
2023-01-18 02:38:24 +0900

    import dense indexer

--- tinysearch/indexers/__init__.py
+++ tinysearch/indexers/__init__.py
@@ -1,2 +1,3 @@
+from tinysearch.indexers.dense_indexer import DenseIndexer  # noqa: F401
 from tinysearch.indexers.indexer import Indexer  # noqa: F401
 from tinysearch.indexers.sparse_indexer import SparseIndexer  # noqa: F401
