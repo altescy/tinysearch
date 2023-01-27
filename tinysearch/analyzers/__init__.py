980dc40dd1cf40ec31d7d3cf3ca07819a17dd9a2
altescy <altescy@fastmail.com>
2023-01-28 00:53:22 +0900

    import pass-through analyzer

--- tinysearch/analyzers/__init__.py
+++ tinysearch/analyzers/__init__.py
@@ -1,2 +1,3 @@
 from tinysearch.analyzers.nltk_analyzer import NltkAnalyzer  # noqa: F401
+from tinysearch.analyzers.passthrough_analyzer import PassThroughAnalyzer  # noqa: F401
 from tinysearch.analyzers.whitespace_tokenizer import WhitespaceTokenizer  # noqa: F401
