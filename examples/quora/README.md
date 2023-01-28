f6ccd726fd04b65dae43a36a3ac47cd800c37256
altescy <altescy@fastmail.com>
2023-01-28 16:32:40 +0900

    add some indexers

--- examples/quora/README.md
+++ examples/quora/README.md
@@ -9,6 +9,13 @@
 | dev  | 79.5    | 80.9      |
 | test | 79.4    | 79.8      |
 
+### ANN Dense Vector Search w/ GloVe [SIF Vectors](https://openreview.net/forum?id=SyK00v5xx)
+
+|      | nDCG@10 | Recall@10 |
+|------|---------|-----------|
+| dev  | 71.8    | 73.6      |
+| test | 72.0    | 72.4      |
+
 ## Usage
 
 Setup python environment:
@@ -22,17 +29,17 @@ pip install -r requirements.txt
 Build index:
 
 ```basdh
-python build.py python build.py outputs/index.pkl
+python build.py --indexer bm25
 ```
 
 Run evaluation script:
 
 ```bash
-python evaluate.py outputs/index.pkl
+python evaluate.py outputs/bm25.pkl
 ```
 
 Search documents interactively:
 
 ```bash
-python search.py outputs/index.pkl
+python search.py outputs/bm25.pkl
 ```
--- examples/quora/build.py
+++ examples/quora/build.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import Any, Dict, Iterator, List, Sequence, Set, Tuple, Union, cast
 
 import ir_datasets
+import minato
 import numpy
 from src.dataloader import DataLoader
 from src.metrics import NDCG, FMeasure, MultiMetrics
@@ -17,18 +18,42 @@ from tinysearch.analyzers import NltkAnalyzer
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("output_filename", type=Path)
-    parser.add_argument("--k1", type=float, default=0.9)
-    parser.add_argument("--b", type=float, default=0.4)
+    parser.add_argument("--output-filename", type=Path, default=None)
+    parser.add_argument("--indexer", choices=["bm25", "sif", "swem"], default="bm25")
     args = parser.parse_args()
 
     dataset_reader = DataLoader(f"beir/quora")
-    searcher = tinysearch.bm25(
-        dataset_reader.load_documents(),
-        analyzer=NltkAnalyzer(stemmer="porter"),
-    )
 
-    searcher.save(args.output_filename)
+    output_filename: Path
+    if args.indexer == "bm25":
+        searcher = tinysearch.bm25(
+            dataset_reader.load_documents(),
+            analyzer=NltkAnalyzer(stemmer="porter"),
+        )
+        output_filename = args.output_filename or (Path("outputs") / "bm25.pkl")
+    elif args.indexer == "sif":
+        searcher = tinysearch.sif(
+            dataset_reader.load_documents(),
+            embeddings=minato.cached_path("https://nlp.stanford.edu/data/glove.6B.zip!glove.6B.300d.txt"),
+            approximate_search=True,
+            analyzer=NltkAnalyzer(),
+            indexer_config={"method": "napp", "space_params": {"numPivot": 1000}},
+        )
+        output_filename = args.output_filename or (Path("outputs") / "sif.pkl")
+    elif args.indexer == "swem":
+        searcher = tinysearch.swem(
+            dataset_reader.load_documents(),
+            embeddings=minato.cached_path("https://nlp.stanford.edu/data/glove.6B.zip!glove.6B.300d.txt"),
+            approximate_search=True,
+            analyzer=NltkAnalyzer(),
+            indexer_config={"method": "napp", "space_params": {"numPivot": 1000}},
+        )
+        output_filename = args.output_filename or (Path("outputs") / "swem.pkl")
+    else:
+        raise ValueError(f"Unknown indexer: {args.indexer}")
+
+    print("Saving index to", output_filename)
+    searcher.save(output_filename)
 
 
 if __name__ == "__main__":
