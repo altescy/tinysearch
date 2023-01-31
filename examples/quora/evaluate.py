465406c318d76623fc6ec827b90e7037016d903c
altescy <altescy@fastmail.com>
2023-01-31 12:14:14 +0900

    add batch option

--- examples/quora/evaluate.py
+++ examples/quora/evaluate.py
@@ -17,6 +17,7 @@ def main() -> None:
     parser.add_argument("tinysearch_filename", type=Path)
     parser.add_argument("--subset", choices=["dev", "test"], default="dev")
     parser.add_argument("--topk", type=int, default=10)
+    parser.add_argument("--batch-size", type=int, default=1)
     args = parser.parse_args()
 
     dataset_reader = DataLoader(f"beir/quora/{args.subset}")
@@ -26,16 +27,20 @@ def main() -> None:
     relations = dataset_reader.load_relations()
     golds = dataset_reader.load_golds()
 
+    num_done = 0
     elapsed_time = 0.0
-    for i, query in enumerate(dataset_reader.load_query(), start=1):
+    for batch in tinysearch.util.batched(dataset_reader.load_query(), args.batch_size):
+        queries = [query["text"] for query in batch]
         start_time = time.time()
-        search_results = searcher.search(query["text"], topk=args.topk)
+        search_results = searcher.search(queries, topk=args.topk)
         elapsed_time += time.time() - start_time
-        gold = golds[query["id"]]
-        pred = [(doc["id"], relations[(query["id"], doc["id"])]) for doc in search_results]
-        metrics(gold, pred)
-        metrics_str = ", ".join(f"{k}={v:.4f}" for k, v in metrics.get_metrics().items())
-        print(f"\r{100 * i/len(golds):6.2f}% speed={i/elapsed_time:.4f}qs/s {metrics_str}", end="")
+        for query, result in zip(batch, search_results):
+            gold = golds[query["id"]]
+            pred = [(doc["id"], relations[(query["id"], doc["id"])]) for doc in result]
+            metrics(gold, pred)
+            num_done += 1
+            metrics_str = ", ".join(f"{k}={v:.4f}" for k, v in metrics.get_metrics().items())
+            print(f"\r{100 * num_done/len(golds):6.2f}% speed={num_done/elapsed_time:.4f}qs/s {metrics_str}", end="")
 
     print()
 
